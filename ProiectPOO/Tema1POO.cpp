9434dcf129d0e8fac882182573d5eb1aeac5b10c
Alexandru <Alyxandru2@gmail.com>
2024-11-21 18:32:00 +0200

    Tema 1 Finished.

--- ProiectPOO/Tema1POO.cpp
+++ ProiectPOO/Tema1POO.cpp
@@ -47,7 +47,8 @@ public:
 			strcpy_s(this->producator,2,"-");
 		}
 	}
-	Autobuz(Autobuz& a):idAutobuz(nrAutobuze) {
+	Autobuz(const Autobuz& a):idAutobuz(nrAutobuze) {
+		nrAutobuze++;
 		this->capacitate = a.capacitate;
 		this->nrPersoaneImbarcate = a.nrPersoaneImbarcate;
 		if (this->producator != NULL) {
@@ -88,7 +89,7 @@ public:
 		
 	}
 	//operator de afisare
-	friend ostream& operator << (ostream& cout, const Autobuz a) {
+	friend ostream& operator << (ostream& cout, const Autobuz& a) {
 		cout << a.idAutobuz << "; " << a.capacitate << "; " << a.nrPersoaneImbarcate << "; ";
 		if (a.producator != NULL) {
 			cout << a.producator;
@@ -99,6 +100,25 @@ public:
 		cout << endl;
 		return cout;
 	}
+	Autobuz& operator =(const Autobuz& a) {
+		if (this != &a) {
+			this->capacitate = a.capacitate;
+			this->nrPersoaneImbarcate = a.nrPersoaneImbarcate;
+			if (this->producator != NULL) {
+				delete[]this->producator;
+			}
+			if (a.producator != NULL) {
+				this->producator = new char[strlen(a.producator) + 1];
+				strcpy_s(this->producator, strlen(a.producator) + 1, a.producator);
+			}
+			else {
+				this->producator = new char[2];
+				strcpy_s(this->producator, 2, "-");
+			}
+		}
+		return *this;
+		
+	}
 
 	int getNumarLocuriLibere() {
 		return (this->capacitate - this->nrPersoaneImbarcate);
@@ -107,17 +127,34 @@ public:
 		return this->capacitate > a.capacitate;
 	}
 
+	explicit operator int() {
+		return this->nrPersoaneImbarcate;
+	}
+	//cast la string
+	operator string () {
+		return this->producator;
+	}
+
 	~Autobuz() {
 		if(this->producator != NULL)
 		delete[]this->producator;
 	}
 	
 };
-int Autobuz::nrAutobuze = 0;
+int Autobuz::nrAutobuze = 1;
 
 int  main() {
 	Autobuz autobuz1;
 	cout << autobuz1;
+	cout << autobuz1.getCapacitate()<<endl;
+
+	char marcel[7] = "Marcel";
+	autobuz1.setProducator(marcel);
+	cout << autobuz1;
+
+
+	cout << autobuz1.getNumarLocuriLibere() << endl;
+
 	char vector[4] = "BMW";
 	Autobuz autobuz2(150, 100, vector);
 	cout << autobuz2;
@@ -127,7 +164,17 @@ int  main() {
 	else{
 		cout << autobuz2.getCapacitate();
 	}
-	cout <<endl<< (autobuz1 > autobuz2);
+	//cout <<endl<< (autobuz1 > autobuz2)<<endl;
+
+	//constructor de copiere
+	Autobuz autobuz3(autobuz2);
+	cout << autobuz3 << autobuz2;
+	autobuz3 = autobuz1;
+	cout << autobuz1 << autobuz3;
+
+	int nrCalatori = /*explicit cast*/ (int)autobuz1;cout << nrCalatori << endl;
+	string producatorulAutobuz = autobuz1;cout << producatorulAutobuz<<endl;
+
 	return 0;
 
 }
